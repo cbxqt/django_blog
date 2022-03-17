5ab391eaec849453a804289a2cc553dbfcb657e1
cbxqt <87889599+cbxqt@users.noreply.github.com>
2022-03-17 14:37:15 +0800

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*@nfl+el8-+nhv5(@p^ku^wn+t%yk^c(z_d)*ud)a*nh#830w1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'blog3',
         'USER': 'root',
-        'PASSWORD': '15829415820.yun',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
